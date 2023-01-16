8bb37a975fb6075b4185bfd09e8c868b75939af2
amitamitvaghamshi9@gmail.comamitgit config --global user.email amitvaghamshi9@gmail.com <amitvaghamshi9@gmail.comgit config --global user.name amit>
2023-01-17 03:36:19 +0530

    mpdified

--- index.js
+++ index.js
@@ -1,5 +1,42 @@
 document.getElementById("subm").addEventListener("click",()=>{
-        alert("Thanks")
-    //   let x=  prompt("enter detail")
-    //   console.log(x);
+    alert("Thanks I will reach you");
+})
+
+
+//for sending email
+function sendmail(){
+    var params={
+        name:document.getElementById("name").value,
+        email:document.getElementById("email").value,
+        message:document.getElementById("message").value,
+    }
+
+    const serviceID="service_yrkuaag";
+    const tampID="template_k5nxr86";
+
+    emailjs.send(serviceID,tampID,params).then(
+        res =>{
+            document.getElementById("name").value="";
+            document.getElementById("amial").value="";
+            document.getElementById("message").value="";
+
+            console.log(res);
+           
+       }).catch((err)=>{
+        console.log("Eror");
+       })
+}
+
+
+
+// //navbar color
+// window.onscroll = function() {
+//     let x=document.getElementById("mydata").scrollHeight
+//     if(window.scrollY>x){
+//     }
+// };
+
+document.getElementById("subm").addEventListener("click",()=>{
+    document.getElementById("subm").style.backgroundImage="linear-gradient(to right, rgb(231, 146, 87) , rgb(83, 195, 121))";
+    document.getElementById("subm").innerText="Success";
 })
